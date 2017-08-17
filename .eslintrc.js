8105a8cac68a205fbbbb44743ed43a714c287400
Ruttkay-Nedecky Peter <Peter.Ruttkay-Nedecky@steinringer.sk>
2017-08-17 10:59:22 +0200

    chore: init repo

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "node": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "sourceType": "module"
+    },
+    "rules": {
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
